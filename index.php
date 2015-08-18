889760c00b74c5a7d6241cb717b6d00c5d43abbb
김남진 <jj4st13@naver.com>
2015-08-18 22:55:15 +0900

    commit

--- index.php
+++ index.php
@@ -6,10 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- 위 3개의 메타 태그는 *반드시* head 태그의 처음에 와야합니다; 어떤 다른 콘텐츠들은 반드시 이 태그들 *다음에* 와야 합니다 -->
-    <title>부트스트랩 101 템플릿</title>
+    <title>KNU 시간표 작성기</title>
 
-    <!-- 부트스트랩 -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME ICONS  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
 
     <!-- IE8 에서 HTML5 요소와 미디어 쿼리를 위한 HTML5 shim 와 Respond.js -->
     <!-- WARNING: Respond.js 는 당신이 file:// 을 통해 페이지를 볼 때는 동작하지 않습니다. -->
@@ -20,29 +24,27 @@
 </head>
 
 <body>
+    <!-- HEADER START -->
     <header>
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <strong>Email: </strong>jj4st13@gmail.com
+                    <strong>컴퓨터정보통신공학11 김남진&nbsp;&nbsp;Email: </strong>jj4st13@gmail.com
                 </div>
-
             </div>
         </div>
     </header>
     <!-- HEADER END-->
 
 
+
+    <!-- LOGO HEADER START-->
     <div class="navbar navbar-inverse set-radius-zero">
         <div class="container">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.html">
-                    <img src="assets/img/logo.png" /></a>
+                <a class="navbar-brand" href="index.php">
+                    <img src="assets/img/logo.png" />
+                </a>
             </div>
 
             <div class="left-div">
@@ -67,34 +69,35 @@
                                 <hr />
                                 <h5><strong>Personal Bio : </strong></h5> Anim pariatur cliche reprehen derit.
                                 <hr />
-                                <a href="#" class="btn btn-info btn-sm">Full Profile</a>&nbsp; <a href="login.html" class="btn btn-danger btn-sm">Logout</a>
+                                <a href="#" class="btn btn-info btn-sm">Full Profile</a>&nbsp; <a href="login.php" class="btn btn-danger btn-sm">Logout</a>
 
                             </div>
                         </li>
-
-
                     </ul>
                 </div>
             </div>
         </div>
     </div>
-
-
     <!-- LOGO HEADER END-->
+
+
+
+    <!-- MENU SECTION START-->
     <section class="menu-section">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <div class="navbar-collapse collapse ">
                         <ul id="menu-top" class="nav navbar-nav navbar-right">
-                            <li><a class="menu-top-active" href="index.html">Dashboard</a></li>
-                            <li><a href="ui.html">free Bord</a></li>
-                            <li><a href="table.html">time-table</a></li>
+                            <li><a class="menu-top-active" href="index.php">Dashboard</a></li>
+                            <li><a href="ui.html">Time-table</a></li>
+                            <li><a href="table.html">Free Bord</a></li>
                             <li><a href="forms.html">QnA</a></li>
-                            <li><a href="login.html">Login Page</a></li>
+                            <li><a href="./login/login.php">Login Page</a></li>
                         </ul>
                     </div>
                 </div>
+
 
             </div>
         </div>
@@ -102,20 +105,30 @@
 
 
     <!-- MENU SECTION END-->
+    fadfadasdasdasdsa<br>
+    fadfadasdasdasdsa<br>
+    fadfadasdasdasdsa<br>
+    fadfadasdasdasdsa<br>
+    fadfadasdasdasdsa<br>
+    fadfadasdasdasdsa<br>
+    fadfadasdasdasdsa<br>
+    fadfadasdasdasdsa<br>
 
-    <!-- CONTENT-WRAPPER SECTION END-->
+
+
+    <!-- FOOTER SECTION START-->
     <footer>
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    &copy; 2015 YourCompany | By : <a href="http://www.designbootstrap.com/" target="_blank">DesignBootstrap</a>
+                    &copy; 2015 KNU 시간표 작성기
                 </div>
 
             </div>
         </div>
     </footer>
     <!-- FOOTER SECTION END-->
-    <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+
 
     <!-- jQuery (부트스트랩의 자바스크립트 플러그인을 위해 필요합니다) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
