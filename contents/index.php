5e66ba10184541987faf62e9ef94f11fed55cf6f
김남진 <jj4st13@naver.com>
2015-08-20 22:05:12 +0900

    test

--- contents/index.php
+++ contents/index.php
@@ -8,12 +8,12 @@
     <!-- 위 3개의 메타 태그는 *반드시* head 태그의 처음에 와야합니다; 어떤 다른 콘텐츠들은 반드시 이 태그들 *다음에* 와야 합니다 -->
     <title>KNU 시간표 작성기</title>
 
-    <!-- BOOTSTRAP CORE STYLE  -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- 부트스트랩 -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
     <!-- FONT AWESOME ICONS  -->
-    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <link href="../assets/css/font-awesome.css" rel="stylesheet" />
     <!-- CUSTOM STYLE  -->
-    <link href="assets/css/style.css" rel="stylesheet" />
+    <link href="../assets/css/style.css" rel="stylesheet" />
 
     <!-- IE8 에서 HTML5 요소와 미디어 쿼리를 위한 HTML5 shim 와 Respond.js -->
     <!-- WARNING: Respond.js 는 당신이 file:// 을 통해 페이지를 볼 때는 동작하지 않습니다. -->
@@ -24,12 +24,11 @@
 </head>
 
 <body>
-    <!-- HEADER START -->
+    <!-- HEADER START-->
     <header>
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <strong>컴퓨터정보통신공학11 김남진&nbsp;&nbsp;Email: </strong>jj4st13@gmail.com
                 </div>
             </div>
         </div>
@@ -43,15 +42,21 @@
     <div class="navbar navbar-inverse set-radius-zero">
         <div class="container">
             <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
                 <a class="navbar-brand" href="index.php">
-                    <img src="assets/img/logo.png" />
+                    <img src="../assets/img/logo.png" />
                 </a>
+
             </div>
 
             <div class="left-div">
                 <div class="user-settings-wrapper">
                     <ul class="nav">
-
                         <li class="dropdown">
                             <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
                                 <span class="glyphicon glyphicon-user" style="font-size: 25px;"></span>
@@ -59,7 +64,7 @@
                             <div class="dropdown-menu dropdown-settings">
                                 <div class="media">
                                     <a class="media-left" href="#">
-                                        <img src="assets/img/64-64.jpg" alt="" class="img-rounded" />
+                                        <img src="../assets/img/64-64.jpg" alt="" class="img-rounded" />
                                     </a>
                                     <div class="media-body">
                                         <h4 class="media-heading">Jhon Deo Alex </h4>
@@ -70,8 +75,7 @@
                                 <hr />
                                 <h5><strong>Personal Bio : </strong></h5> Anim pariatur cliche reprehen derit.
                                 <hr />
-                                <a href="#" class="btn btn-info btn-sm">Full Profile</a>&nbsp; <a href="login.php" class="btn btn-danger btn-sm">Logout</a>
-
+                                <a href="#" class="btn btn-info btn-sm">Full Profile</a>&nbsp; <a href="../login/login.php" class="btn btn-danger btn-sm">Logout</a>
                             </div>
                         </li>
                     </ul>
@@ -84,7 +88,6 @@
 
 
 
-
     <!-- MENU SECTION START-->
     <section class="menu-section">
         <div class="container">
@@ -93,14 +96,15 @@
                     <div class="navbar-collapse collapse ">
                         <ul id="menu-top" class="nav navbar-nav navbar-right">
                             <li><a class="menu-top-active" href="index.php">Dashboard</a></li>
-                            <li><a href="ui.html">Time-table</a></li>
-                            <li><a href="table.html">Free Bord</a></li>
-                            <li><a href="forms.html">QnA</a></li>
-                            <li><a href="./login/login.php">Login Page</a></li>
+                            <li><a href="table.php">Time-table</a></li>
+                            <li><a href="board.php">Free board</a></li>
+                            <li><a href="qna.php">QnA</a></li>
+                            <li>
+                                <a type="button" data-toggle="modal" data-target="#modal_login">login</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
-
 
             </div>
         </div>
@@ -111,11 +115,18 @@
 
 
 
-    <!-- CONTENT-WRAPPER SECTION START-->
-
-
-
-    <!-- CONTENT-WRAPPER SECTION END-->
+    <!-- CONTENTS SECTION START-->
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <!-- CONTENTS SECTION END-->
 
 
 
@@ -126,7 +137,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    &copy; 2015 KNU 시간표 작성기
+                    &copy; 2015 YourCompany | By : <a href="http://www.designbootstrap.com/" target="_blank">DesignBootstrap</a>
                 </div>
 
             </div>
@@ -135,10 +146,40 @@
     <!-- FOOTER SECTION END-->
 
 
+    <!-- Modal -->
+    <div class="modal fade" id="modal_login" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Login</h4>
+                </div>
+
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="user_id">user ID</label>
+                        <input type="email" class="form-control" id="user_id" placeholder="User ID" />
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="Password" />
+                    </div>
+                    <button type="submit" class="butn butn-warning butn-block">login</button>
+                    <button type="submit" class="butn butn-warning butn-block">Sign in</button>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     <!-- jQuery (부트스트랩의 자바스크립트 플러그인을 위해 필요합니다) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <!-- 모든 컴파일된 플러그인을 포함합니다 (아래), 원하지 않는다면 필요한 각각의 파일을 포함하세요 -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
 </body>
 
 </html>
