5995c11ee95340dd0e4e7bc59d45eaec1c55b2bf
jungin500 <ji5489@gmail.com>
2015-08-26 21:12:26 +0900

    한글화 및 추가 필드 수정

--- lib/modal.php
+++ lib/modal.php
@@ -44,24 +44,44 @@
 
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Register</h4>
+                <h4 class="modal-title" id="myModalLabel">회원가입</h4>
             </div>
 
             <div class="modal-body">
                 <form method='post' action='login/registerHandler.php'>
                     <div class="form-group">
-                        <label for="name">Name</label>
-                        <input type="name" name="username" class="form-control" id="username" placeholder="Username" />
-                    </div>
-                    <div class="form-group">
-                        <label for="user_id">user ID</label>
+                        <label for="user_id">아이디</label>
                         <input type="id" name="userid" class="form-control" id="userid" placeholder="User ID" />
                     </div>
                     <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="userpw" class="form-control" id="userpw" placeholder="Password" />
+                        <label for="name">이름</label>
+                        <input type="text" name="username" class="form-control" id="username" placeholder="예: 홍길동" />
                     </div>
-                    <button type="submit" class="btn btn-warning btn-block"><i class="glyphicon glyphicon-log-in"></i> Register</button>
+                    <div class="form-group">
+                        <label for="password">비밀번호</label>
+                        <input type="password" name="userpw" class="form-control" id="userpw" placeholder="예: 4~20자리의 숫자,영대/소문자 및 특수기호" />
+                    </div>
+                    <div class="form-group">
+                        <label for="user_num">학번</label>
+                        <input type="number" name="usernum" class="form-control" id="usergrade" placeholder="예: 201500000" />
+                    </div>
+                    <div class="form-group">
+                        <label for="user_age">나이</label>
+                        <input type="number" name="userage" class="form-control" id="userage" placeholder="예: 20" />
+                    </div>
+                    <div class="form-group">
+                        <label for="user_birth">생일</label>
+                        <input type="date" name="userbirth" class="form-control" id="userbirth" placeholder="1996-01-01" />
+                    </div>
+                    <div class="form-group">
+                        <label for="user_id">학년</label>
+                        <input type="number" name="usergrade" class="form-control" id="usergrade" placeholder="예: 3" />
+                    </div>
+                    <div class="form-group">
+                        <label for="user_id">자기소개</label>
+                        <input type="text" name="usercomment" class="form-control" id="usercomment" placeholder="예: 안녕하세요. 홍길동입니다." />
+                    </div>
+                    <button type="submit" class="btn btn-warning btn-block"><i class="glyphicon glyphicon-log-in"></i> 가입</button>
                 </form>
             </div>
             <div class="modal-footer">
--- login/registerHandler.php
+++ login/registerHandler.php
@@ -20,17 +20,25 @@
 	$dbpass = "loginpw";
 	$dbname ="db1";
 	$dbhost = "localhost";
+    $dbtable = "usertest1";
 
 	$sqlConn = mysql_connect($dbhost, $dbid, $dbpass);
 	mysql_select_db($dbname, $sqlConn);
 
 	//아이디와 비밀번호의 값을 POST방식으로 받는 것
+    //'$num', '$id', '$pw', '$token', '$name', '$age', '$birthdate', '$grade', '$comment'
+    $num = $_POST['usernum'];
 	$id = $_POST['userid'];
+	$pw = $_POST['userpw'];
 	$name = $_POST['username'];
-	$pass = $_POST['userpw'];
+	$age = $_POST['userage'];
+	$birthdate = $_POST['userbirth'];
+	$grade = $_POST['usergrade'];
+	$comment = $_POST['usercomment'];
+
 
 	//입력받은 id가 이미 존재하는지 체크
-	$getID = "SELECT id FROM usertest1 WHERE id='$id'";
+	$getID = "SELECT id FROM $dbtable WHERE id='$id'";
 	$getID = mysql_query($getID);
 	$getID = mysql_fetch_array($getID);
 
@@ -39,34 +47,34 @@
         $result = "ID EXISTS!";
 	} else { // ID가 존재하지 않는 경우
 
-		//아이디와 비밀번호 및 기타 정보들을 DB에 등록한다.
-		$setDATA = "INSERT INTO usertest1 (num, id, password, token, name, age, birthdate, grade, comment) VALUES (NULL, '$id', '$pw', NULL, '$name', NULL, NULL, NULL, NULL)";
-        mysql_query($setDATA);
-
 		//64자리의 무작위 문자열을 생성한다.
 		//이 64자리의 임의의 수가 바로 토큰으로 로그인 대조에 사용할 키 값.
 		$key = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789^/';
 		for($i=0;$i<=63;$i++)
             $token = $key[rand(0,63)];
 
+		//아이디와 비밀번호 및 기타 정보들을 DB에 등록한다.
+		$setDATA = "INSERT INTO $dbtable (num, id, password, token, name, age, birthdate, grade, comment) VALUES ('$num', '$id', '$pw', '$token', '$name', '$age', '$birthdate', '$grade', '$comment')";
+        $setDATA = mysql_query($setDATA);
+        echo "result: " . $setDATA . "\n";
+
         //방금 만든 토큰을 데이터베이스에 업데이트한다.
 		//입력받은 아이디가 있는 위치에 업데이트.
-		$updateToken = "UPDATE usertest1 SET token='$token' WHERE id='$id'";
+		$updateToken = "UPDATE $dbtable SET token='$token' WHERE id='$id'";
 		$updateToken = mysql_query($updateToken);
+        echo "result: " . $updateToken . "\n";
 
         //세션에 토큰 즉 키 값을 등록한다.
 		$_SESSION['token'] = $token;
 
         //이름 등 각종 정보를 세션에 저장한다.
-        $_SESSION['desc'] = $desc;
+        $_SESSION['desc'] = $comment;
         $_SESSION['userid'] = $id;
         $_SESSION['username'] = $name;
 
         //홈 페이지로 리디렉트 후 종료한다.
         echo "<meta http-equiv='refresh' content='0;url=../index.php'>";
-
 	}
 
-    echo $result;
-    return 1;
+    return 0;
 ?>
