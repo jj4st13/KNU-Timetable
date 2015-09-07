5e3a913c2cd39fe643d8d67647a5309dacba315f
jungin500 <jungin500@naver.com>
2015-09-08 07:44:43 +0900

    시간표 구성 (진행중)

--- contents/table.php
+++ contents/table.php
@@ -7,43 +7,8 @@
         </div>
 
         <div class="row">
+            <!--  시간표 작성 -->
             <div class="col-md-6">
-                <!-- 시간표 출력 -->
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <b>수강 신청 목록</b>
-                    </div>
-
-                    <div class="panel-body" style="font-size: 12px">
-                        <div class="table-responsive">
-                            <table class="table table-condensed table-striped table-bordered table-hover">
-                                <thead>
-                                    <tr>
-                                        <th>신청</th>
-                                        <th>구분</th>
-                                        <th>분반</th>
-                                        <th>과목명</th>
-                                        <th>시수</th>
-                                        <th>대상학과</th>
-                                        <th>정원</th>
-                                        <th>담당교수</th>
-                                        <th>강의실</th>
-                                        <th>비고</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-6">
-                <!--   시간표 작성 -->
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <b>전체 강의 목록</b>
@@ -74,11 +39,9 @@
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="row">
+            <!-- 시간표 출력 -->
             <div class="col-md-6">
-                <!-- 시간표 출력 -->
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <b>시간표</b>
@@ -99,8 +62,51 @@
                                 </thead>
                                 <tbody>
                                     <tr>
-                                        <th>1교시<br />09:00~10:15<br />09:00~09~50</th>
+                                        <th>1교시
+                                            <br /><b>09:00~10:15</b>
+                                            <br />09:00~09:50</th>
                                     </tr>
+                                    <tr>
+                                        <th>2교시
+                                            <br /><b>10:30~11:45</b>
+                                            <br />10:00~10:50</th>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <!-- 수강 목록 출력 -->
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <b>수강 신청 목록</b>
+                    </div>
+
+                    <div class="panel-body" style="font-size: 12px">
+                        <div class="table-responsive">
+                            <table class="table table-condensed table-striped table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>신청</th>
+                                        <th>구분</th>
+                                        <th>분반</th>
+                                        <th>과목명</th>
+                                        <th>시수</th>
+                                        <th>대상학과</th>
+                                        <th>정원</th>
+                                        <th>담당교수</th>
+                                        <th>강의실</th>
+                                        <th>비고</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
                                 </tbody>
                             </table>
                         </div>
