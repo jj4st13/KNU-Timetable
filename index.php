3af5493e3f4680889bb00011a7561dd4365c4e58
jungin500 <jungin500@naver.com>
2015-09-07 23:42:57 +0900

    상대경로 수정(정리)

--- index.php
+++ index.php
@@ -5,16 +5,16 @@ ini_set("display_errors", 1);
         session_start();
     }
 
-    require_once "./contents/upper.php";    //상단
+    require_once "contents/upper.php";    //상단
     if(!isset($_GET[ "pageid"]) || $_GET[ "pageid"]=="dashboard")
-        require_once "./contents/dashboard.php";
+        require_once "contents/dashboard.php";
     else if(isset($_GET[ "pageid"]) && $_GET[ "pageid"]=="table")
-        require_once "./contents/table.php";
+        require_once "contents/table.php";
     else if(isset($_GET[ "pageid"]) && $_GET[ "pageid"]=="board")
-        require_once "./contents/board.php";
+        require_once "contents/board.php";
     else if(isset($_GET[ "pageid"]) && $_GET[ "pageid"]=="qna")
-        require_once "./contents/qna.php";
-    require_once "./lib/modal.php";         // 모달 로그인
+        require_once "contents/qna.php";
+    require_once "lib/modal.php";         // 모달 로그인
 ?>
 
     <!-- FOOTER SECTION START-->
@@ -34,6 +34,6 @@ ini_set("display_errors", 1);
     <!-- jQuery (부트스트랩의 자바스크립트 플러그인을 위해 필요합니다) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <!-- 모든 컴파일된 플러그인을 포함합니다 (아래), 원하지 않는다면 필요한 각각의 파일을 포함하세요 -->
-    <script src="./assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
     </body>
 </html>
